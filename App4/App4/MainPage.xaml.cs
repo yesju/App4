cbb5dc291b938e65b980211f23d9514f3e85b8e7
omx1 <omx1@YESICA>
2017-09-18 20:27:37 -0500

    Add project files.

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            guardar.Clicked += guardar_Clicked;
+        }
+        private void guardar_Clicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert("Datos Ingresados", "Bienvenida \n" + Nombre.Text + "\n" + Apellidos.Text + "" + Direccion.Text + "\n" + Telefono.Text + "\n" + Carrera.Text + "\n" + Semestre.Text + "\n" + Matricula.Text + "\n" + Correo.Text + "\n" + GitHub.Text, "OK");
+        }
+    }
+}
